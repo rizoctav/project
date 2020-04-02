991475c1fe9cbb38afd3f0fa4f8f9d475d78c413
rizoctav <63040910+rizoctav@users.noreply.github.com>
2020-04-02 15:19:54 +0700

    Add files via upload

---
+++ android/app/src/main/java/com/jepunfood/app_hci/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jepunfood.app_hci;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
